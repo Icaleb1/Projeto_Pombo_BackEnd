cee7f5cf6f0fa3c6e6f793ecb8a1971f82608731
Icaleb1 <calebfernandes080@gamil.com>
2024-10-25 08:51:16 -0300

    first commit

---
+++ src/main/java/senac/projeto/pombo/pombo/ProjetoPomboBackEndApplication.java
@@ -0,0 +1,13 @@
+package senac.projeto.pombo.pombo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPomboBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPomboBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/senac/projeto/pombo/pombo/ProjetoPomboBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package senac.projeto.pombo.pombo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPomboBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
